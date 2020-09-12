0283e61ade9e7cc637a2586ba5847df3488f7b9e
Andimeo <andimeo@foxmail.com>
2020-09-13 03:06:20 +0800

    more pythonic

--- 52.PermutedMultiples.py
+++ 52.PermutedMultiples.py
@@ -1,6 +1,6 @@
 from collections import Counter
 
 for x in range(1, 10**6):
-    if Counter(str(x)) == Counter(str(x*2)) == Counter(str(x*3)) == Counter(str(x*4)) == Counter(str(x*5)) == Counter(str(x*6)):
+    if all(Counter(str(x)) == Counter(str(x*y)) for y in range(2, 7)):
         print(x)
         break
